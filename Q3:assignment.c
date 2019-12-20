277871696a781d0e3a4da16d008e7ef69d158746
Shivu_2708 <50650966+shivanip27@users.noreply.github.com>
2019-12-20 17:11:46 +0530

    Update Q3:assignment.c

--- Q3:assignment.c
+++ Q3:assignment.c
@@ -5,11 +5,13 @@ main()
 	int arr1[SIZE], N_arr[SIZE],i,j,k,c=0,p=0;
 	
 	printf("Enter the array element: ");
+	
 	for(i=0;i<SIZE;i++)
 	scanf("%d",&arr1[i]);
 	
 	for(i=0;i<SIZE;i++)
 	printf("%d ",arr1[i]);
+	
 	printf("\n");
 	
 	for(k=0;k<SIZE;k++)
@@ -18,11 +20,11 @@ main()
 	{
 		
 		if(arr1[k] % j ==0)
-	{
+	  {
 		c=1;
 		break;
-	}
-    }
+	  }
+        }
 		if(c==0)
 		{
 			N_arr[p]=arr1[i];
